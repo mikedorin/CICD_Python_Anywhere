33bdf91180e8700be531e7f8827ac479fa350a1d
mikedorin <78502248+mikedorin@users.noreply.github.com>
2025-10-16 13:28:13 -0500

    a quick test

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ tasks = [
 
 @app.route('/')
 def home():
-    return "Welcome to the DevOps Demo App!! Version 2"
+    return "Welcome to the DevOps Demo App!! Version 3"
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
