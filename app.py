f8a97ed506f54721d1d8d42dde07d42f8c03f48b
Michael Dorin <mike.dorin@stthomas.edu>
2026-03-19 18:55:12 -0500

    changed my version.

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ tasks = [
 
 @app.route('/')
 def home():
-    return "Welcome to the DevOps Demo App!! Version 111. Changed the version."
+    return "Welcome to the DevOps Demo App!! Version 2. Changed the version."
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
