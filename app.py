575df3a6125c7902fc69627719a278fbacb78116
Michael Dorin <mike.dorin@stthomas.edu>
2026-03-19 19:37:24 -0500

    made a small change to demo

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ tasks = [
 
 @app.route('/')
 def home():
-    return "Welcome to the DevOps Demo App!! Version 4...."
+    return "Welcome to the DevOps Demo App!! Version 5...."
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
